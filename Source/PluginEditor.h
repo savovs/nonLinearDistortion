6031999664bcd093219f6ccfb81a05268adab4de
David Moore <jdm@Johns-MacBook-Pro-2.local>
2017-02-21 15:06:22 +0000

    Initial Commit
    Starting point code

---
+++ Source/PluginEditor.h
@@ -0,0 +1,71 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Projucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Projucer version: 4.3.0
+
+  ------------------------------------------------------------------------------
+
+  The Projucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright (c) 2015 - ROLI Ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCE_HEADER_4256DE31A32D2019__
+#define __JUCE_HEADER_4256DE31A32D2019__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Projucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class NonLinearAudioProcessorEditor  : public AudioProcessorEditor,
+                                       public Timer
+{
+public:
+    //==============================================================================
+    NonLinearAudioProcessorEditor (NonLinearAudioProcessor& p);
+    ~NonLinearAudioProcessorEditor();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    void timerCallback();
+    //[/UserMethods]
+
+    void paint (Graphics& g) override;
+    void resized() override;
+
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    NonLinearAudioProcessor& processor;
+    //[/UserVariables]
+
+    //==============================================================================
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NonLinearAudioProcessorEditor)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // __JUCE_HEADER_4256DE31A32D2019__
